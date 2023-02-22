0b6a6c24cf82de9fbfe530c2683a3b57bbaf1789
Mariusz Politowicz <politowiczmar@gmail.com>
2023-02-22 19:40:14 +0100

    Add files pulled from Kodilla

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+
+import "./styles/normalize.scss";
+import "./styles/global.scss";
+import "font-awesome/css/font-awesome.min.css";
+
+const container = document.getElementById("root");
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  container
+);
